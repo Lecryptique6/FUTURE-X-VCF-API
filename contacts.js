019206b85f1bc9b9767357db8bb12daefcbb17de
Lecryptique6 <romarioskenne6@gmail.com>
2025-08-24 07:17:31 +0100

    Delete contacts.js

--- contacts.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ContactSchema = new mongoose.Schema({
-  prenom: { type: String, required: true },
-  numero: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Contact', ContactSchema);
